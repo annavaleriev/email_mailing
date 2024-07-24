bc5bdf1682f22031dac4adf728bdc907a0a9822e
annavaleriev <filenko.a@gmail.com>
2024-07-24 14:27:59 +0300

    Написаны модели для рассылки

--- services/models.py
+++ services/models.py
@@ -1,3 +1,84 @@
 from django.db import models
 
-# Create your models here.
+from services.utils import NULLABLE
+
+
+class Client(models.Model):
+    """Модель для хранения данных о клиентах"""
+
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, verbose_name="Фамилия")
+    surname = models.CharField(max_length=50, verbose_name="Отчество")
+    email = models.EmailField(verbose_name="Почта")
+    comment = models.TextField(verbose_name="Комментарий", **NULLABLE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+
+class Message(models.Model):
+    """Модель для хранения данных о сообщениях"""
+
+    subject = models.CharField(max_length=150, verbose_name="Тема письма")
+    body = models.TextField(verbose_name="Тело письма")
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
+
+
+class SendMail(models.Model):
+    """Модель для хранения данных о письмах"""
+
+    PERIODICITY_CHOICES = (
+        ('once', 'Один раз в день'),
+        ('weekly', 'Один раз в неделю'),
+        ('monthly', 'Один раз в месяц'),
+    )
+
+    STATUS_MAIL = (
+        ("created", "Создано"),
+        ("sent", "Отправлено"),
+        ("end", "Завершено"),
+    )
+
+    date_start_send = models.DateTimeField(verbose_name="Дата первой отправки рассылки")
+    date_end_send = models.DateTimeField(verbose_name="Дата окончания отправки")
+    time_first_send = models.TimeField(verbose_name="Время первой отправки рассылки")
+    periodicity = models.CharField(max_length=10, choices=PERIODICITY_CHOICES, verbose_name="Периодичность рассылки")
+    status = models.CharField(max_length=10, choices=STATUS_MAIL, default="created", verbose_name="Статус рассылки")
+    subject = models.CharField(max_length=150, verbose_name="Тема")
+    # message = models.TextField(verbose_name="Сообщение")
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name="Сообщение")
+
+    # client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name="Клиент")
+    client = models.ManyToManyField(Client, related_name="client", verbose_name="Клиент")
+
+    def __str__(self):
+        return f"Рассылка: {self.subject} - Статус: {self.status}"
+
+    class Meta:
+        verbose_name = "Рассылка"
+        verbose_name_plural = "Рассылки"
+
+
+class Logs(models.Model):
+    """Модель для хранения логов"""
+
+    date_and_time_last_send = models.DateTimeField(verbose_name="Дата и время последней отправки")
+    status_send = models.BooleanField(default=False, verbose_name="Статус отправки")
+    error_message = models.TextField(max_length=200, verbose_name="Сообщение об ошибке", **NULLABLE)
+
+    def __str__(self):
+        return f"Дата рассылки: {self.date_and_time_last_send} - Статус: {self.status_send}"
+
+    class Meta:
+        verbose_name = "Лог"
+        verbose_name_plural = "Логи"
