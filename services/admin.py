025a0daf2867366834961fbdb68af2376584c6b2
annavaleriev <filenko.a@gmail.com>
2024-07-28 17:59:21 +0300

    Сделан шаблон для главной страницы Home и настройки

--- services/admin.py
+++ services/admin.py
@@ -2,7 +2,7 @@ from distutils.log import Log
 
 from django.contrib import admin
 
-from services.models import Client, SendMail, Message
+from services.models import Client, Logs, Message, SendMail
 
 
 @admin.register(Client)
@@ -10,9 +10,9 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "surname", "email", "comment", "is_blocked")
     search_fields = ("first_name", "last_name", "surname", "email", "is_blocked")
     list_filter = ("first_name", "last_name", "surname", "email", "comment", "is_blocked")
-    ordering = ('last_name',)  # Сортировка по фамилии
-    list_editable = ('is_blocked',)  # Редактирование статуса блокировки клиента прямо там
-    readonly_fields = ('email',)  # Поле только для чтения, нельзя менять email
+    ordering = ("last_name",)  # Сортировка по фамилии
+    list_editable = ("is_blocked",)  # Редактирование статуса блокировки клиента прямо там
+    readonly_fields = ("email",)  # Поле только для чтения, нельзя менять email
     list_per_page = 20
     search_help_text = "Лучше искать клиента  фамилии или email клиента"
 
@@ -26,25 +26,31 @@ class MessageAdmin(admin.ModelAdmin):
 
 @admin.register(SendMail)
 class SendMailAdmin(admin.ModelAdmin):
-    list_display = ("date_start_send", "periodicity", "status", "message", "clients")
-    search_fields = ("date_start_send", "periodicity", "status", "message", "clients")
-    list_filter = ("date_start_send", "periodicity", "status", "message", "clients")
-    readonly_fields = ('date_start_send',)  # Поле только для чтения, нельзя менять дату отправки
-    ordering = ('date_start_send',)
+    list_display = ("date_start_send", "periodicity", "status", "message", "get_clients")
+    search_fields = ("date_start_send", "periodicity", "status", "message")
+    list_filter = ("date_start_send", "periodicity", "status", "message")
+    readonly_fields = ("date_start_send",)  # Поле только для чтения, нельзя менять дату отправки
+    ordering = ("date_start_send",)
     list_per_page = 30
-    list_editable = ('status',)  # Редактирование статуса рассылки, только нафига это надо?
+    list_editable = ("status",)  # Редактирование статуса рассылки, только нафига это надо?
     search_help_text = "Лучше искать рассылку по дате и клиенту"
     fieldsets = (
-        (None, {
-            'fields': ('date_start_send', 'periodicity', 'status')
-        }),
-        ('Message & Clients', {
-            'fields': ('message', 'clients'),
-        }),
+        (None, {"fields": ("date_start_send", "periodicity", "status")}),
+        (
+            "Message & Clients",
+            {
+                "fields": ("message", "clients"),
+            },
+        ),
     )  # Группировка полей в админке, не знаю что  это, но вот и посмотрю
 
+    def get_clients(self, obj):  # Это метод для вывода всех клиентов в рассылке
+        return ", ".join([client.email for client in obj.clients.all()])  # Вывод всех клиентов в рассылке
 
-@admin.register(Log)
+    get_clients.short_description = "Клиенты"  # Название колонки в админке
+
+
+@admin.register(Logs)
 class LogAdmin(admin.ModelAdmin):
     list_display = ("date_and_time_last_send", "status_send", "server_message")
     search_fields = ("date_and_time_last_send", "status_send", "server_message")
--- services/views.py
+++ services/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
-# Create your views here.
+from services.models import Client, Message
+
+
+# ТУТ ЕЩЕ ЧТО-ТО С КОНТАКСТОМ ДОЛЖНО БЫТЬ, СМОТРИ ПРЕД ДЗ
+class HomeView(ListView):
+    model = Message
+    template_name = "services/home.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Сервис рассылок"
+        context["text"] = "Добро пожаловать на сайт с рассылками"
+        return context
+
+
+class ClientListView(ListView):
+    model = Client
+    template_name = "services/client_list.html"
+    context_object_name = "clients"
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = "services/client_detail.html"
+    context_object_name = "client"
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    template_name = "services/client_create.html"
+    fields = "__all__"
+    success_url = reverse_lazy("services:client_list")
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    template_name = "services/client_update.html"
+    fields = "__all__"
+    context_object_name = "client"
+    success_url = reverse_lazy("services:client_list")
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = "services/client_delete.html"
+    context_object_name = "client"
+    success_url = reverse_lazy("services:client_list")
+
+
+# class MessageListView(ListView):
+#     model = Message
+#     template_name = "services/message_list.html"
+#     context_object_name = "message"
